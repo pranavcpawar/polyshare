34dffaf1534b6988876e4c9b8a61c78ebf468e4a
pranavcpawar <pranavcpawar@outlook.com>
2023-07-23 23:59:55 +0530

    adds connectkit with dependencies

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,7 +1,21 @@
+"use client";
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ['latin'] })
+
+const config = createConfig(
+	getDefaultConfig({
+		// Required API Keys
+		alchemyId: process.env.ALCHEMY_API_KEY,
+		walletConnectProjectId: process.env.WALLET_CONNECT_PROJECT_ID,
+
+		// Required
+		appName: "polyshare",
+	})
+);
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +25,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <WagmiConfig config={config}>
+        <ConnectKitProvider mode="dark">
+          <body className={inter.className}>{children}</body>
+        </ConnectKitProvider>
+      </WagmiConfig>
     </html>
   );
 };
