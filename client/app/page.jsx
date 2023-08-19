dacda1f6e43d8c6e48b2553763e206a8176123bf
pranavcpawar <pranavcpawar@outlook.com>
2023-08-19 16:13:08 +0530

    move metadata from layout to page

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,6 +1,11 @@
 "use client";
 import Image from "next/image";
 
+export const metadata = {
+  title: 'polyshare',
+  description: 'A Decentralized approach to share amongst peer',
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
