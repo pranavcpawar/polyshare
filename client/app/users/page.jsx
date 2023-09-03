e8940170d5721f046cc3f485a8e3542ccdaa5690
pranavcpawar <pranavcpawar@outlook.com>
2023-09-03 21:48:51 +0530

    add users page

---
+++ client/app/users/page.jsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+  title: 'polyshare - all users',
+};
+
+export default function Users() {
+  return (
+    <main className="grid place-items-center w-full fixed p-5">
+     <div>
+      <h1>Users Page</h1>
+     </div>
+    </main>
+  );
+};
