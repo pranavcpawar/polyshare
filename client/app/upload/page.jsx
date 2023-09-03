099ba5f881942433fed604033c2e248b0d206d9e
pranavcpawar <pranavcpawar@outlook.com>
2023-09-03 21:48:30 +0530

    add upload page

---
+++ client/app/upload/page.jsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+  title: 'polyshare - upload data',
+};
+
+export default function Upload() {
+  return (
+    <main className="grid place-items-center w-full fixed p-5">
+      <div>
+        <h1>Upload Page</h1>
+      </div>
+    </main>
+  );
+};
