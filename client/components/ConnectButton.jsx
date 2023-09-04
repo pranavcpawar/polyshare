10a82ee5b2131e4ee0fa4e13a1b884d52748fab1
pranavcpawar <pranavcpawar@outlook.com>
2023-09-04 23:51:01 +0530

    fix connect wallet button

--- client/components/ConnectButton.jsx
+++ client/components/ConnectButton.jsx
@@ -7,7 +7,7 @@ const ConnectButton = () => {
       <ConnectKitButton.Custom>
         {({ isConnected, truncatedAddress, show }) => {
           return (
-            <button onClick={show} className="bg-[#161618] text-[rgb(60,100,159)] font-bold w-[132px] h-[40px] outline outline-2 outline-offset-0 outline-[#212121] hover:bg-[#191919] rounded-[10px]">
+            <button onClick={show} className="bg-[#161618] font-bold w-[132px] h-[40px] outline outline-2 outline-offset-0 text-[#656565] hover:text-[rgba(60,100,159)] outline-[#212121] hover:scale-110 transition-transform duration-200 ease-out rounded-[12px]">
               {isConnected ? truncatedAddress : "Connect Wallet"}
             </button>
           );
