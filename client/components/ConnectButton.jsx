958548cb8e0edf500882daa61f2910673ee987d8
pranavcpawar <pranavcpawar@outlook.com>
2023-09-07 23:43:25 +0530

    remame connect wallet button to Connect

--- client/components/ConnectButton.jsx
+++ client/components/ConnectButton.jsx
@@ -7,7 +7,7 @@ const ConnectButton = () => {
         {({ isConnected, truncatedAddress, show }) => {
           return (
             <button onClick={show} className="bg-[#090909] text-lg font-poppins w-[132px] h-[48px] outline outline-2 outline-offset-0 hover:text-[rgba(60,100,159)] hover:bg-[#101010] outline-[#212121] active:scale-95 transition-transform ease-in-out duration-200 rounded-[15px]">
-              {isConnected ? truncatedAddress : "Connect Wallet"}
+              {isConnected ? truncatedAddress : "Connect"}
             </button>
           );
         }}
