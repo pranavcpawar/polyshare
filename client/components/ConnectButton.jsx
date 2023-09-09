546742a3b6b92fed3d5b66c46d036d9cc589c730
pranavcpawar <pranavcpawar@outlook.com>
2023-09-09 23:42:32 +0530

    changes connect button design

--- client/components/ConnectButton.jsx
+++ client/components/ConnectButton.jsx
@@ -6,7 +6,7 @@ const ConnectButton = () => {
       <ConnectKitButton.Custom>
         {({ isConnected, truncatedAddress, show }) => {
           return (
-            <button onClick={show} className="bg-[#090909] text-lg font-poppins w-[132px] h-[48px] outline outline-2 outline-offset-0 hover:text-[rgba(60,100,159)] hover:bg-[#101010] outline-[#212121] active:scale-95 transition-transform ease-in-out duration-200 rounded-[15px]">
+            <button onClick={show} className="bg-[rgba(60,100,159)] text-lg font-poppins font-semibold w-[132px] h-[48px] outline outline-2 outline-offset-0 hover:bg-[rgba(60,100,159,0.6)] text-background outline-black active:scale-95 transition-transform ease-in-out duration-200 rounded-[15px]">
               {isConnected ? truncatedAddress : "Connect"}
             </button>
           );
