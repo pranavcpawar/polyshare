1590ff5b643b4eb4d0ab1156b78decee9cdcc3ca
pranavcpawar <pranavcpawar@outlook.com>
2023-09-12 23:54:38 +0530

    initial users page

--- client/app/users/page.jsx
+++ client/app/users/page.jsx
@@ -8,7 +8,7 @@ export default function Users() {
   return (
     <main className="grid place-items-center w-full fixed p-5">
      <div>
-      <h1>Users Page</h1>
+      
      </div>
     </main>
   );
